26518cdafe7e4be627d12f70bf6a8c2978cc47ef
struts2spring <struts2spring@gmail.com>
2014-01-12 02:20:12 +0530

    first commit

--- HibernateTraining/src/com/example/domain/User.java
+++ HibernateTraining/src/com/example/domain/User.java
@@ -3,9 +3,9 @@ package com.example.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "USER")
