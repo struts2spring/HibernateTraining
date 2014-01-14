2a70e9d6d1376b20075d94b8aab2b449f943249f
struts2spring <struts2spring@gmail.com>
2014-01-15 01:45:39 +0530

    oscache

---
+++ Cache_OSCache/test/com/example/training/test/AppTest.java
@@ -0,0 +1,41 @@
+package com.example.training.test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import com.example.training.entity.DepartmentEntity;
+import com.example.training.util.HibernateUtil;
+
+public class AppTest {
+
+	@Test
+	public void testOSCache() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction txn = session.beginTransaction();
+
+		// fetch the department entity from database first time
+		DepartmentEntity department = (DepartmentEntity) session.load(DepartmentEntity.class, new Integer(1));
+		System.out.println(department.getName());
+
+		// fetch the department entity again; Fetched from first level cache
+		department = (DepartmentEntity) session.load(DepartmentEntity.class, new Integer(1));
+		System.out.println(department.getName());
+
+		// Let's close the session
+		session.getTransaction().commit();
+		session.close();
+
+		// Try to get department in new session
+		Session anotherSession = HibernateUtil.getSessionFactory().openSession();
+		anotherSession.beginTransaction();
+
+		// Here entity is already in second level cache so no database query
+		// will be hit
+		department = (DepartmentEntity) anotherSession.load(DepartmentEntity.class, new Integer(1));
+		System.out.println(department.getName());
+
+		txn.commit();
+		session.close();
+	}
+}
